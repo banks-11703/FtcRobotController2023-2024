54d2b4bc10f3a841bd676549b2169f697384171d
Smidoodles <smidoodles@gmail.com>
2023-10-07 12:45:42 -0700

    Sum of Friday changes-primarily pid tuning a test motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PidTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PidTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -7,13 +8,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.MotorControlAlgorithm;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-import java.util.concurrent.TimeUnit;
 
 @TeleOp
 @Config
@@ -22,9 +21,12 @@ public class PidTest extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotorEx spinner = null;
     private boolean buttonAWasPressed = false;
-    public static double Kp = 0;
-    public static double Ki = 0;
+    public static double Kp = 10;
+    public static double Ki = 3;
     public static double Kd = 0;
+    public static double Kf = 0;
+    public static int target = 0;
+
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
@@ -32,25 +34,13 @@ public class PidTest extends LinearOpMode {
         spinner = hardwareMap.get(DcMotorEx.class, "spinnyThing");
         FtcDashboard dashboard = FtcDashboard.getInstance();
         Telemetry dashboardTelemetry = dashboard.getTelemetry();
-        dashboardTelemetry.addData("Velocity", spinner.getVelocity());
-        dashboardTelemetry.addData("Target",0);
+        dashboardTelemetry.addData("Reality", spinner.getCurrentPosition());
+        dashboardTelemetry.addData("Target", 0);
         dashboardTelemetry.update();
-        spinner.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        spinner.setTargetPosition(target);
+        spinner.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        spinner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-
-
-//        long integralSumLimit = Math.round(0.25 / Ki);
-//
-        double reference = 0;
-//        double lastReference = reference;
-//
-//        double integralSum = 0;
-//
-//        double error = 0;
-//        double lastError = 0;
-//        double encoderPosition = 0;
-//        double derivative = 0;
-//        double out = 0;
 
         waitForStart();
         runtime.reset();
@@ -59,87 +49,30 @@ public class PidTest extends LinearOpMode {
         while (opModeIsActive() && !isStopRequested()) {
 
             if (gamepad1.a && !buttonAWasPressed) {
-                if (reference == 0) {
-                    reference = 2000;
+                if (target == 0) {
+                    target = 2000;
+                    spinner.setPower(1);
                 } else {
-                    reference = 0;
+                    target = 0;
+                    spinner.setPower(0);
                 }
                 buttonAWasPressed = true;
             } else if (!gamepad1.a && buttonAWasPressed) {
                 buttonAWasPressed = false;
             }
-            spinner.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(Kp,Ki,Kd,0, MotorControlAlgorithm.LegacyPID));
-            spinner.setVelocity(reference);
-            spinner.setPower(1);
-            telemetry.addData("Velocity: ",spinner.getVelocity());
-            telemetry.addData("Target",reference);
+            spinner.setPIDCoefficients(DcMotorEx.RunMode.RUN_TO_POSITION, new PIDCoefficients(Kp, Ki, Kd));
+            spinner.setTargetPosition(target);
+
+            telemetry.addData("Reality: ", spinner.getCurrentPosition());
+            telemetry.addData("Target", target);
             telemetry.update();
 
 
-            dashboardTelemetry.addData("Velocity", spinner.getVelocity());
-            dashboardTelemetry.addData("Target",reference);
+            dashboardTelemetry.addData("Reality", spinner.getCurrentPosition());
+            dashboardTelemetry.addData("Target", target);
+            dashboardTelemetry.addData("Power", spinner.getPower());
+//            dashboardTelemetry.addData("Pid: ",spinner.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER));
             dashboardTelemetry.update();
-//            // Elapsed timer class from SDK, please use it, it's epic
-//            ElapsedTime timer = new ElapsedTime();
-//
-//            while (spinner.getVelocity() != reference && !isStopRequested()) {
-//
-//                if (gamepad1.a && !buttonAWasPressed) {
-//                    if (reference == 0) {
-//                        reference = 2000;
-//                    } else {
-//                        reference = 0;
-//                    }
-//                    buttonAWasPressed = true;
-//                } else if (!gamepad1.a && buttonAWasPressed) {
-//                    buttonAWasPressed = false;
-//                }
-//
-//
-//                // obtain the encoder position
-//                encoderPosition = spinner.getVelocity();
-//                // calculate the error
-//                error = reference - encoderPosition;
-//
-//                // rate of change of the error
-//                derivative = (error - lastError) / timer.time();
-//
-//                // sum our integral
-//                integralSum = integralSum + (error * timer.time(TimeUnit.SECONDS));
-//
-////                // set a limit on our integral sum
-////                if (integralSum > integralSumLimit) {
-////                    integralSum = integralSumLimit;
-////                }
-////                if (integralSum < -integralSumLimit) {
-////                    integralSum = -integralSumLimit;
-////                }
-//
-//                out = (Kp * error) + (Ki * integralSum) + (Kd * derivative);
-//
-//                spinner.setPower(out);
-//
-//                lastError = error;
-//
-//                // reset the timer for next time
-//                timer.reset();
-//
-//                lastReference = reference;
-//                // reset the integral if the reference is changed.
-//                if (reference != lastReference) {
-//                    integralSum = 0;
-//                }
-//                telemetry.addData("Velocity: ",spinner.getVelocity());
-//                telemetry.addData("Reference: ",reference);
-//                telemetry.addData("Error:",error);
-//                telemetry.addData("Power: ",out);
-//                telemetry.addData("InLoop: ",true);
-//                telemetry.update();
-//            }
-//            telemetry.addData("Velocity: ",spinner.getVelocity());
-//            telemetry.addData("Error: ",error);
-//            telemetry.addData("Power: ",out);
-//            telemetry.update();
         }
     }
 }
